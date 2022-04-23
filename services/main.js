ef0ed407ce66e2215ffb381564ea5fd64d4c83c8
ulldecorb <ausias.bertran@gmail.com>
2022-04-23 15:48:10 +0200

    Add delay on fade in  path elements

--- services/main.js
+++ services/main.js
@@ -2,25 +2,31 @@
 const svg = document.getElementById('svg');
 const fadeInDelay = [ 700, 1400, 2100, 2800, 3500, 4200, 4900, 5600 ];
 const pathsize = 'M 100 190 l 90 -90 l -90 -90 l -90 90 l 90 90 z';
-const fadeOutSnakes = () => console.log(svg.outerHTML);
+const fadeOutSnakes = () => {
+    console.log(svg.outerHTML);
+
+    svg.style.width = '150vmax';
+    svg.style.height = '150vmax';
+
+}
 
 
-(function createSnakes () {
-    for ( i = 0; i < 2; i ++) {
-        // const spanSize = snakeSize[i]
-        const delay = fadeInDelay[i];
-        const path = document.createElement("path");
-        path.setAttribute("d", pathsize);
-        path.setAttribute("stroke", "#6e5c0c");
-        path.setAttribute("stroke-width", 3);
-        path["stroke-width"] = 3;
-        path.setAttribute("fill", "transparent");
-        path.classList.add('snake');
+// (function createSnakes () {
+//     for ( i = 0; i < 2; i ++) {
+//         // const spanSize = snakeSize[i]
+//         const delay = fadeInDelay[i];
+//         const path = document.createElement("path");
+//         path.setAttribute("d", pathsize);
+//         path.setAttribute("stroke", "#6e5c0c");
+//         path.setAttribute("stroke-width", 3);
+//         path["stroke-width"] = 3;
+//         path.setAttribute("fill", "transparent");
+//         path.classList.add('snake');
 
-        setTimeout(() => {
-            svg.appendChild(path);
-        }, delay);
-    }
-})()
+//         setTimeout(() => {
+//             svg.appendChild(path);
+//         }, delay);
+//     }
+// })()
 
 document.body.addEventListener("click", fadeOutSnakes);
